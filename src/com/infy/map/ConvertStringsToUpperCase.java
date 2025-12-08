e61eef95b24a7727eac498ef19cdbc792d0070dd
PavanUngarala <pavankumarungarala@gmail.com>
2025-12-08 22:30:28 +0530

    First commit and initiate the entire project structure

---
+++ src/com/infy/map/ConvertStringsToUpperCase.java
@@ -0,0 +1,18 @@
+//Convert list of strings to uppercase
+package com.infy.map;
+
+import java.util.List;
+
+public class ConvertStringsToUpperCase {
+
+	public static void main(String[] args) {
+		List<String> names = List.of("alice", "bob", "charlie");
+		System.out.println("List of strings before modified");
+		names.forEach(name -> System.out.print(name + "  "));
+		names = names.stream().map(name -> name.toUpperCase()).toList();
+		System.out.println();
+		System.out.println("List of strings after modified by Streams");
+		names.forEach(name -> System.out.print(name + "  "));
+	}
+
+}
